0717fe54b16fedc6227cf2eac672ab5410499aa9
cris4crisland <160337227+cris4crisland@users.noreply.github.com>
2024-04-04 11:21:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomCardValue = Math.floor(Math.random() * 13);
+  const cardValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomCardSuits = Math.floor(Math.random() * 4);
+  const cardSuits = ["♦", "♥", "♠", "♣"];
+  let cardValue = document.querySelector(".cardValue");
+  cardValue.innerHTML = cardValues[randomCardValue];
+  let cardSuit = document.querySelectorAll("#suits");
+  for (let index = 0; index < cardSuit.length; index++) {
+    cardSuit[index].classList.add(cardSuits[randomCardSuits]);
+    cardSuit[index].innerHTML = cardSuits[randomCardSuits];
+  }
 };
